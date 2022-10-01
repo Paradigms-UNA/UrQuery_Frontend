0676a04098c047d14e9abb8fad7038cc4ecd59a8
albnacho <albcarlosrndn@gmail.com>
2022-10-01 11:46:26 -0600

    Added other base files
    Work has been divided between JAndres and Carlos
    
    Added the packages: Monaco-editor & axios for the frontend

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { Navbar } from './components/Navbar';
 
-function App() {
+/**
+ * TODO Components to program:
+ * -> Navbar exists but, we have to do the click button logic (the aboutService.js)
+ * -> DA make from 0, also do the documentService.js, which should connect with the /document/DDDD service 
+ *    -- Must render the load button which triggers the call to the service
+ * 
+ * -> EA make from 0, also do the compileService.js file.
+ *     --- must render the compile button, which triggers logic
+ *  
+ * -> RA make from 0, must listen to the change of the value in EA. 
+ *    -- Probably using "useContext" hook
+ *    -- After EA and DA are complete
+ * 
+ */
+
+const App = () => {
   return (
     <div className='container-fluid'>
       <Navbar />
